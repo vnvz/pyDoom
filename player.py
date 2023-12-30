c616232138706e365fa8aa56d9cba7549e848737
vnvz <vvv@cesar.school>
2023-12-30 18:59:49 -0300

    adicionar engine de raycasting e pseudo 3D

--- player.py
+++ player.py
@@ -1,3 +1,4 @@
+# Desc: This is the player class. It handles the player's movement and drawing.
 from settings import *
 import pygame as pg
 import math
@@ -49,9 +50,9 @@ class Player:
             self.y += dy
         
     def draw(self):
-        pg.draw.line(self.game.screen, 'yellow', (self.x * 100, self.y * 100),
-                     (self.x * 100 + WIDTH * math.cos(self.angle),
-                     self.y * 100 + WIDTH * math.sin(self.angle)), 2)
+        # pg.draw.line(self.game.screen, 'yellow', (self.x * 100, self.y * 100),
+        #              (self.x * 100 + WIDTH * math.cos(self.angle),
+        #              self.y * 100 + WIDTH * math.sin(self.angle)), 2)
         pg.draw.circle(self.game.screen, 'green', (self.x * 100, self.y * 100), 15)
         
     def update(self):
--- settings.py
+++ settings.py
@@ -1,5 +1,10 @@
+import math
+
+# Desc: Settings for the game
 # settings
 RES = WIDTH, HEIGHT = 1600, 900
+HALF_WIDTH = WIDTH // 2
+HALF_HEIGHT = HEIGHT // 2
 FPS = 0
 
 # player
@@ -7,3 +12,14 @@ PLAYER_POS = 1.5, 5 #mini_map
 PLAYER_ANGLE = 0
 PLAYER_SPEED = 0.004
 PLAYER_ROT_SPEED = 0.002
+
+# ray casting
+FOV = math.pi / 3
+HALF_FOV = FOV / 2
+NUM_RAYS = WIDTH // 2
+HALF_NUM_RAYS = NUM_RAYS // 2
+DELTA_ANGLE = FOV / NUM_RAYS
+MAX_DEPTH = 20
+
+SCREEN_DIST = HALF_WIDTH / math.tan(HALF_FOV)
+SCALE = WIDTH // NUM_RAYS
